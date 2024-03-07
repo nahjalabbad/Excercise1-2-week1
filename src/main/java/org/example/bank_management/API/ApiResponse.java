24a4c1090d42976a2fdbcaf759c88cdfcdf97eca
nahjalabbad <146772725+nahjalabbad@users.noreply.github.com>
2024-03-07 17:15:18 +0300

    Add files via upload

---
+++ src/main/java/org/example/bank_management/API/ApiResponse.java
@@ -0,0 +1,10 @@
+package org.example.bank_management.API;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String messageStatus;
+}
